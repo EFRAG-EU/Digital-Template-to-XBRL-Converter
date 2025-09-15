adac228fe663d1f991dada7c0e55d15fef813735
Stuart Rowan <stuart.rowan@strr.org>
2025-09-15 16:48:25 +0100

    Increase number of expected facts now that we create boolean facts for False cell values.

--- tests/integrationTests/test_expectedFactCounts.py
+++ tests/integrationTests/test_expectedFactCounts.py
@@ -28,8 +28,8 @@ skip_if_not_main_unless_forced = pytest.mark.skipif(
 IX_NAMESPACE = "http://www.xbrl.org/2013/inlineXBRL"
 
 TEST_CASES = [
-    ("tests/data/VSME-Digital-Template-Sample-1.0.0.xlsx", 139),
-    ("tests/data/VSME-Digital-Template-Sample-1.0.1.xlsx", 139),
+    ("tests/data/VSME-Digital-Template-Sample-1.0.0.xlsx", 142),
+    ("tests/data/VSME-Digital-Template-Sample-1.0.1.xlsx", 142),
     ("tests/data/vsme-unit-test-v1.0.0.xlsx", 159),
 ]
 
